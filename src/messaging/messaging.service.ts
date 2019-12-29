e4aed27183f625bbedf7979f0ebd496e435a904a
thefirstspine <teddy@coretizone.com>
2019-12-29 12:22:46 +0100

    Logged messages from messaging service

--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import fetch, { Response } from 'node-fetch';
 import env from '../@shared/env-shared/env';
+import { LogService } from 'src/@shared/log-shared/log.service';
 
 @Injectable()
 export class MessagingService {
@@ -8,7 +9,10 @@ export class MessagingService {
   static readonly SUBJECT__QUEUE: string = 'TheFirstSpine:queue';
   static readonly SUBJECT__GAME: string = 'TheFirstSpine:game';
 
+  constructor(private readonly logService: LogService) {}
+
   async sendMessage(to: number[]|'*', subject: string|'*', message: any): Promise<IMessagingResponse> {
+    this.logService.info('Send message to messaging service', {to, subject, message});
     const response: Response = await fetch(env.config.MESSAGING_INTERNAL_URL + '/api', {
       body: JSON.stringify({
         to,
@@ -22,6 +26,7 @@ export class MessagingService {
       },
     });
     const jsonResponse = await response.json();
+    this.logService.info('Response from messaging service', jsonResponse);
     return jsonResponse;
   }
 
