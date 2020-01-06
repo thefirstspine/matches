f997fb991856a73a8f55390fb9a4adeb2551cd20
thefirstspine <teddy@coretizone.com>
2020-01-06 09:51:19 +0100

    Added helper to merge items & loots

--- src/utils/game.utils.ts
+++ src/utils/game.utils.ts
@@ -1,4 +1,5 @@
-import { IHistoryItem } from '../@shared/arena-shared/wizzard';
+import { IHistoryItem, IWizzardItem } from '../@shared/arena-shared/wizzard';
+import { ILoot } from '../@shared/rest-shared/entities';
 
 /**
  * Get the score of the user based on his history
@@ -8,4 +9,20 @@ export function getScore(history: IHistoryItem[]): number {
   return history.reduce((value: number, historyItem: IHistoryItem) => {
       return value + (historyItem.victory ? 3 : -1);
   }, 0);
+}
+
+/**
+ * Add loots in an item list.
+ * @param items
+ * @param loot
+ */
+export function mergeLootsInItems(items: IWizzardItem[], loot: ILoot[]) {
+  loot.forEach((loot: ILoot) => {
+    const item: IWizzardItem|undefined = items.find((i: IWizzardItem) => i.name === loot.name);
+    if (item !== undefined) {
+      item.num += loot.num;
+    } else {
+      items.push(JSON.parse(JSON.stringify(loot)));
+    }
+  });
 }
