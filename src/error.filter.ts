ea66a8b5b04d968f96fb594a24b0b638768c1e10
thefirstspine <teddy@coretizone.com>
2020-01-06 21:20:45 +0100

    Better error support

--- src/error.filter.ts
+++ src/error.filter.ts
@@ -1,5 +1,6 @@
-import { ExceptionFilter, Catch, HttpException, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { LogService } from './@shared/log-shared/log.service';
+import { Response } from 'express';
 
 @Catch()
 export class ErrorFilter implements ExceptionFilter {
@@ -7,13 +8,33 @@ export class ErrorFilter implements ExceptionFilter {
     private readonly logService: LogService,
   ) {}
 
-  catch(error: Error, host: ArgumentsHost) {
-    const status = (error instanceof HttpException) ? error.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
-    this.logService.error(
-      `Global error with status ${status}`, {
-        message: error.message,
-        name: error.name,
-        stack: error.stack,
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    if (status >= 500) {
+      this.logService.error(
+        `Global error with status ${status}`, {
+          message: exception.message,
+          name: exception.name,
+          stack: exception.stack,
+        });
+    } else {
+      this.logService.warning(
+        `Global warning with status ${status}`, {
+          message: exception.message,
+          name: exception.name,
+          stack: exception.stack,
+        });
+    }
+
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
       });
   }
 }
