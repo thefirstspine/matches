347c63e4d34ceaa164dffbe0749c3858817607da
thefirstspine <teddy@coretizone.com>
2020-01-08 21:44:50 +0100

    Added doc to service

--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -3,6 +3,10 @@ import fetch, { Response } from 'node-fetch';
 import env from '../@shared/env-shared/env';
 import { LogService } from '../@shared/log-shared/log.service';
 
+/**
+ * Main service to interact with the "messaging" service. The messaging service will
+ * send data to users through sockets in real time.
+ */
 @Injectable()
 export class MessagingService {
 
@@ -11,6 +15,12 @@ export class MessagingService {
 
   constructor(private readonly logService: LogService) {}
 
+  /**
+   * Send messages to users
+   * @param to
+   * @param subject
+   * @param message
+   */
   async sendMessage(to: number[]|'*', subject: string|'*', message: any): Promise<IMessagingResponse> {
     this.logService.info('Send message to messaging service', {to, subject, message});
     const response: Response = await fetch(env.config.MESSAGING_INTERNAL_URL + '/api', {
@@ -35,7 +45,11 @@ export class MessagingService {
 
 }
 
+/**
+ * Interface representing a messaging's response
+ */
 export interface IMessagingResponse {
-  sent: boolean;
+  status: boolean;
+  original: any;
   error?: string;
 }
