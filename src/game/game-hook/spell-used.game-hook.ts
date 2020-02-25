24b740e355b7ae9a20938bf31604771ff1c333d4
thefirstspine <teddy@coretizone.com>
2020-02-25 08:56:20 +0100

    Added Insane's Echo effect

--- src/game/game-hook/spell-used.game-hook.ts
+++ src/game/game-hook/spell-used.game-hook.ts
@@ -10,7 +10,17 @@ import { IGameInstance, IGameCard, IGameAction } from '../../@shared/arena-share
 @Injectable()
 export class SpellUsedGameHook implements IGameHook {
 
-  async execute(gameInstance: IGameInstance, params: any): Promise<boolean> {
+  async execute(gameInstance: IGameInstance, params: {gameCard: IGameCard}): Promise<boolean> {
+    // Add strength to the Insane's Echo card
+    gameInstance.cards
+      .filter((card: IGameCard) => card.location === 'board' && card.user === params.gameCard.user && card.card.id === 'insanes-echo')
+      .forEach((card: IGameCard) => {
+        card.currentStats.bottom.strength += 2;
+        card.currentStats.left.strength += 2;
+        card.currentStats.right.strength += 2;
+        card.currentStats.top.strength += 2;
+      });
+
     // Get the spells in the hand & delete the associated actions
     // TODO: filter here with an Ether
     gameInstance.cards
