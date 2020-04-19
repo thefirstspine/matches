4b94b7daaee42dd1f3a0dd56e05cbff444767678
thefirstspine <teddy@coretizone.com>
2020-04-19 22:22:50 +0200

    Fixed Curse of Mara discard

--- src/game/game-worker/throw-cards.game-worker.ts
+++ src/game/game-worker/throw-cards.game-worker.ts
@@ -184,7 +184,7 @@ export class ThrowCardsGameWorker implements IGameWorker, IHasGameHookService {
       .filter((card: IGameCard) => {
         return card.user === user && card.location === 'hand';
       })
-      .map((card: IGameCard, index: number) => card.id !== 'curse-of-mara' ? index : null)
+      .map((card: IGameCard, index: number) => card.card.id !== 'curse-of-mara' ? index : null)
       .filter((i) => i !== null);
   }
 }
