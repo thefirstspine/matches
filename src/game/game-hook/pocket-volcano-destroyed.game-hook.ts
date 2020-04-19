7390e02398934ab1910cad81e382b33dc874fb64
thefirstspine <teddy@coretizone.com>
2020-04-19 15:15:39 +0200

    Added effects of Jellyfish & Pocket Volcano

---
+++ src/game/game-hook/pocket-volcano-destroyed.game-hook.ts
@@ -0,0 +1,44 @@
+import { IGameHook } from './game-hook.interface';
+import { Injectable } from '@nestjs/common';
+import { IGameInstance, IGameCard } from '../../@shared/arena-shared/game';
+import { randBetween } from '../../utils/maths.utils';
+import { RestService } from '../../rest/rest.service';
+import { ICard } from '../../@shared/rest-shared/card';
+
+/**
+ * This subscriber is executed once a 'card:destroyed:pocket-volcano' event is thrown.
+ * @param gameInstance
+ * @param params
+ */
+@Injectable()
+export class PocketVolcanoDestroyedGameHook implements IGameHook {
+
+  public constructor(private readonly restService: RestService) {}
+
+  async execute(gameInstance: IGameInstance, params: {gameCard: IGameCard}): Promise<boolean> {
+    // Test for possibility to put a ditch card
+    if (!gameInstance.cards.find((c: IGameCard) => {
+      return c.location === 'board' &&
+        c.coords.x === params.gameCard.coords.x &&
+        c.coords.y === params.gameCard.coords.y &&
+        (['creature', 'artifact'].includes(c.card.type) || ['burden-earth', 'ditch'].includes(c.card.id));
+    })) {
+      const ditch: ICard = await this.restService.card('lava');
+      const randomId: number = randBetween(0, Number.MAX_SAFE_INTEGER);
+      const ditchGameCard: IGameCard = {
+        card: ditch,
+        id: `${gameInstance}_${randomId}`,
+        location: 'board',
+        user: 0,
+        coords: {
+          x: params.gameCard.coords.x,
+          y: params.gameCard.coords.y,
+        },
+      };
+      gameInstance.cards.push(ditchGameCard);
+    }
+
+    return true;
+  }
+
+}
