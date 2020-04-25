4ca3abeee465fcaedb81e4ded7d614ee4977d43a
thefirstspine <teddy@coretizone.com>
2020-04-25 16:55:50 +0200

    Include game changes

--- src/game/game-worker/game-worker.interface.ts
+++ src/game/game-worker/game-worker.interface.ts
@@ -1,4 +1,4 @@
-import { IGameAction, IGameInstance } from '../..//@shared/arena-shared/game';
+import { IGameAction, IGameInstance, anySubaction } from '../..//@shared/arena-shared/game';
 
 export interface IGameWorker {
 
@@ -11,33 +11,33 @@ export interface IGameWorker {
    * Method to create an action (called by the Game service & other workers)
    * @param gameInstance
    */
-  create(gameInstance: IGameInstance, data: {[key: string]: any}): Promise<IGameAction|null>;
+  create(gameInstance: IGameInstance, data: {[key: string]: any}): Promise<IGameAction<anySubaction>|null>;
 
   /**
    * This is called for each remaining actions after a possible action succeed and is deleted.
    * @param gameInstance
    * @param gameAction
    */
-  refresh(gameInstance: IGameInstance, gameAction: IGameAction): Promise<void>;
+  refresh(gameInstance: IGameInstance, gameAction: IGameAction<anySubaction>): Promise<void>;
 
   /**
    * Executes the action. When succeed, this action is deleted & the other actions will be refreshed.
    * @param gameInstance
    * @param gameAction
    */
-  execute(gameInstance: IGameInstance, gameAction: IGameAction): Promise<boolean>;
+  execute(gameInstance: IGameInstance, gameAction: IGameAction<anySubaction>): Promise<boolean>;
 
   /**
    * The action expired. Succeed that to delete it.
    * @param gameInstance
    * @param gameAction
    */
-  expires(gameInstance: IGameInstance, gameAction: IGameAction): Promise<boolean>;
+  expires(gameInstance: IGameInstance, gameAction: IGameAction<anySubaction>): Promise<boolean>;
 
   /**
    * Deleted the action from the instance.
    * @param gameInstance
    * @param gameAction
    */
-  delete(gameInstance: IGameInstance, gameAction: IGameAction): Promise<void>;
+  delete(gameInstance: IGameInstance, gameAction: IGameAction<anySubaction>): Promise<void>;
 }
