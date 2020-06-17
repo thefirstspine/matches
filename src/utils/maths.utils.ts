5827d3e5747ccfe10accf16e9b836d1156af5b61
thefirstspine <teddy@coretizone.com>
2020-06-17 19:39:33 +0200

    Added some comments about utils

--- src/utils/maths.utils.ts
+++ src/utils/maths.utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Give a random interger between two values
+ * @param min
+ * @param max
+ */
 export function randBetween(min: number, max: number) {
   return Math.floor(Math.random() * max) + min;
 }
