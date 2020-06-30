1870f2724d77bc7dbced2630a03059b79832e808
thefirstspine <teddy@coretizone.com>
2020-06-30 22:35:12 +0200

    Handled unlock of triumph "transporter"

--- src/game/game-hook/monstrous-portal-damaged.game-hook.ts
+++ src/game/game-hook/monstrous-portal-damaged.game-hook.ts
@@ -1,8 +1,10 @@
 import { IGameHook } from './game-hook.interface';
 import { Injectable } from '@nestjs/common';
-import { IGameInstance, IGameCard, IGameAction } from '@thefirstspine/types-arena';
+import { IGameInstance, IGameCard, IGameAction, IWizard } from '@thefirstspine/types-arena';
 import { IHasGameWorkerService } from '../injections.interface';
 import { GameWorkerService } from '../game-worker/game-worker.service';
+import { WizzardService } from '../../wizzard/wizzard.service';
+import { WizzardsStorageService } from '../../storage/wizzards.storage.service';
 
 /**
  * This subscriber is executed once a 'card:lifeChanged:damaged:monstrous-portal' event is thrown.
@@ -12,10 +14,25 @@ import { GameWorkerService } from '../game-worker/game-worker.service';
 @Injectable()
 export class MonstrousPortalDamagedGameHook implements IGameHook, IHasGameWorkerService {
 
+  constructor(
+    private readonly wizardService: WizzardService,
+    private readonly wizzardsStorageService: WizzardsStorageService,
+  ) {}
+
   public gameWorkerService: GameWorkerService;
 
   async execute(gameInstance: IGameInstance, params: {gameCard: IGameCard, source: IGameCard, lifeChanged: number}): Promise<boolean> {
     if (params.lifeChanged <= -1) {
+
+      // Unlock title "transporter"
+      if (params.source.user === params.gameCard.user) {
+        const wizard: IWizard = this.wizardService.getWizzard(params.gameCard.user);
+        if (wizard && !wizard.triumphs.includes('transporter')) {
+          wizard.triumphs.push('transporter');
+          this.wizzardsStorageService.save(wizard);
+        }
+      }
+
       const action: IGameAction<any> = await this.gameWorkerService.getWorker('monstrous-portal-effect')
         .create(gameInstance, {user: params.gameCard.user});
       if (
