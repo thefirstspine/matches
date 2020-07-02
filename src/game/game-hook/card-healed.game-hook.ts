12884a7a636b4ced57a620bfd2c37b05f3aa0a66
thefirstspine <teddy@coretizone.com>
2020-07-02 12:00:07 +0200

    Added effects of Anvil of Xiarm'ha

--- src/game/game-hook/card-healed.game-hook.ts
+++ src/game/game-hook/card-healed.game-hook.ts
@@ -16,7 +16,7 @@ export class CardHealedGameHook implements IGameHook {
     private readonly messagingService: MessagingService,
   ) {}
 
-  async execute(gameInstance: IGameInstance, params: {gameCard: IGameCard, lifeChanged: number}): Promise<boolean> {
+  async execute(gameInstance: IGameInstance, params: {gameCard: IGameCard, source: IGameCard|null, lifeChanged: number}): Promise<boolean> {
     await this.messagingService.sendMessage(
       gameInstance.users.map((u: IGameUser) => u.user),
       `TheFirstSpine:game:${gameInstance.id}:cardChanged`,
@@ -27,10 +27,22 @@ export class CardHealedGameHook implements IGameHook {
         gameCard: params.gameCard,
       });
 
-    if (params.gameCard.currentStats.life > params.gameCard.card.stats.life) {
+    if (this.shouldLimitMaxLife(params.gameCard, params.source) && params.gameCard.currentStats.life > params.gameCard.card.stats.life) {
       params.gameCard.currentStats.life = params.gameCard.card.stats.life;
     }
 
+    return true;
+  }
+
+  protected shouldLimitMaxLife(target: IGameCard, source: IGameCard|null): boolean {
+    if (target.card.id === 'anvil-of-xiarmha' && source === null) {
+      // Anvil of Xiarm'ha can have more than the starting value in case of new turn
+      return false;
+    }
+    if (target.card.id === 'anvil-of-xiarmha' && source.card.id === 'reinforcement') {
+      // Anvil of Xiarm'ha can have more than the starting value when reinforcement is used
+      return false;
+    }
     return true;
   }
 
