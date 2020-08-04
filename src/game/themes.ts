8eee100e6aced600c36204c0cb73109e005123ac
thefirstspine <teddy@coretizone.com>
2020-08-04 17:39:53 +0200

    Added theme "sacrifice church"

--- src/game/themes.ts
+++ src/game/themes.ts
@@ -3,12 +3,14 @@ export class Themes {
   static readonly DEAD_FOREST: string = 'dead-forest';
   static readonly SPINE_S_CAVE: string = 'spine-s-cave';
   static readonly FORGOTTEN_CEMETERY: string = 'forgotten-cemetery';
+  static readonly SACRIFICE_CHURCH: string = 'sacrifice-church';
 
   static get all(): string[] {
     return [
       Themes.DEAD_FOREST,
       Themes.SPINE_S_CAVE,
       Themes.FORGOTTEN_CEMETERY,
+      Themes.SACRIFICE_CHURCH,
     ];
   }
 
@@ -16,6 +18,7 @@ export class Themes {
     return [
       Themes.DEAD_FOREST,
       Themes.SPINE_S_CAVE,
+      Themes.SACRIFICE_CHURCH,
     ];
   }
 
