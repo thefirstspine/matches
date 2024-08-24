2ba7f5bdd122c956fee0b85ab7e1d1d2f3e43ff3
TheFirstSpine <teddy@thefirstspine.fr>
2024-08-24 09:38:56 +0200

    feat(rooms): remove public rooms

--- src/rooms/arena-rooms.service.ts
+++ src/rooms/arena-rooms.service.ts
@@ -14,12 +14,6 @@ export class ArenaRoomsService {
   constructor(
       private readonly roomsService: RoomsService,
   ) {
-    this.roomsService.createRoom(
-      ArenaRoomsService.SUBJECT,
-      {
-        name: 'realm-' + (process.env.REALM ? process.env.REALM : ''),
-        senders: [],
-      });
   }
 
   /**
@@ -52,32 +46,11 @@ export class ArenaRoomsService {
       });
   }
 
-  async joinPublicRoom(user: number, language: 'fr'|'en') {
-    return this.roomsService.joinRoom(
-      ArenaRoomsService.SUBJECT,
-      this.getRoomNameForGeneral(language),
-      {
-        user,
-        displayName: '---',
-      });
-  }
-
-  async leavePublicRoom(user: number, language: 'fr'|'en') {
-    return this.roomsService.leaveRoom(
-      ArenaRoomsService.SUBJECT,
-      this.getRoomNameForGeneral(language),
-      user);
-  }
-
   /**
    * Get the room name for a given game
    * @param game
    */
   protected getRoomNameForGame(game: IGameInstance): string {
     return `game-${game.id}`;
-  }
-
-  protected getRoomNameForGeneral(language: 'fr'|'en'): string {
-    return `general-${language}`;
   }
 }
